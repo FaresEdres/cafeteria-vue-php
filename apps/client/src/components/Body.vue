a37127e8f7d8399bbba142eae9671cf21092ead9
Rahma Mostafa <rahmamostafa282@gmail.com>
2025-04-02 00:28:36 +0200

    add home page styling

---
+++ apps/client/src/components/Body.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="super_container">
+    <div class="intro">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="intro_content">
+						<div class="intro_subtitle page_subtitle">Something new</div>
+						<div class="intro_title"><h2>An Extraordinery Experience</h2></div>
+						<div class="intro_text">
+							<p>Welcome to <span class="heart">Armene Café</span> <span class="small-heart">&#10084;</span> , where every moment is an experience of warmth and flavor. Our café is a sanctuary for coffee lovers, food enthusiasts, and anyone seeking a cozy escape. From expertly brewed coffee to handcrafted pastries, every dish is made with passion and care. Whether you're enjoying a morning cup or relaxing in the evening, you'll find that every detail at Armene Café is designed for your comfort and enjoyment. Join us for a truly memorable experience!</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xl-4 col-md-6 intro_col">
+							<div class="intro_image"><img src="/intro_1.jpg" alt="https://unsplash.com/@quanle2819"></div>
+						</div>
+						<div class="col-xl-4 col-md-6 intro_col">
+							<div class="intro_image"><img src="/intro_2.jpg" alt="https://unsplash.com/@fabmag"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  </div>
+</template>
+
+
+
+
+<script>
+export default {
+  name: "BodyComponent",
+
+};
+</script>
+
+
+<style scoped>
+  .small-heart {
+  font-size: 18px;
+  color: rgba(255, 0, 0, 0.3);
+  margin-left: 5px;
+}
+</style>
